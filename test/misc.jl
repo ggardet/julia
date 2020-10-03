6fbe9488ebc80aadf7f0e6dfc1e39cdd711dfafb
Kristoffer <kcarlsson89@gmail.com>
2020-10-03 18:20:28 +0200

    disable assert macro unless running in debug mode

--- test/misc.jl
+++ test/misc.jl
@@ -3,64 +3,8 @@
 isdefined(Main, :FakePTYs) || @eval Main include("testhelpers/FakePTYs.jl")
 
 # Tests that do not really go anywhere else
-
-# test @assert macro
-@test_throws AssertionError (@assert 1 == 2)
-@test_throws AssertionError (@assert false)
-@test_throws AssertionError (@assert false "this is a test")
-@test_throws AssertionError (@assert false "this is a test" "another test")
-@test_throws AssertionError (@assert false :a)
-let
-    try
-        @assert 1 == 2
-        error("unexpected")
-    catch ex
-        @test isa(ex, AssertionError)
-        @test occursin("1 == 2", ex.msg)
-    end
-end
-# test @assert message
-let
-    try
-        @assert 1 == 2 "this is a test"
-        error("unexpected")
-    catch ex
-        @test isa(ex, AssertionError)
-        @test ex.msg == "this is a test"
-    end
-end
-# @assert only uses the first message string
-let
-    try
-        @assert 1 == 2 "this is a test" "this is another test"
-        error("unexpected")
-    catch ex
-        @test isa(ex, AssertionError)
-        @test ex.msg == "this is a test"
-    end
-end
-# @assert calls string() on second argument
-let
-    try
-        @assert 1 == 2 :random_object
-        error("unexpected")
-    catch ex
-        @test isa(ex, AssertionError)
-        @test !occursin("1 == 2", ex.msg)
-        @test occursin("random_object", ex.msg)
-    end
-end
-# if the second argument is an expression, c
-let deepthought(x, y) = 42
-    try
-        @assert 1 == 2 string("the answer to the ultimate question: ",
-                              deepthought(6, 9))
-        error("unexpected")
-    catch ex
-        @test isa(ex, AssertionError)
-        @test ex.msg == "the answer to the ultimate question: 42"
-    end
-end
+assert_file = joinpath(@__DIR__, "assert.jl")
+@test success(`$(Base.julia_cmd()) -g2 $assert_file`)
 
 let # test the process title functions, issue #9957
     oldtitle = Sys.get_process_title()
